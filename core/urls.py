775e81adfe01cfdbe0f0ceb35ca819f528d29d81
Tech-Wolf07 <yammu2005@gmail.com>
2025-09-07 15:35:52 +0530

    UI modifications

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
     path('course/<int:course_id>/generate-qr/', views.generate_qr_code, name='generate_qr_code'),
     path('course/<int:course_id>/report/', views.view_report, name='view_report'),
     path('mark-attendance/', views.mark_attendance, name='mark_attendance'),
-    path('login/', views.login_view, name='login'),
     path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
     path('student/scan-qr/', views.scan_qr_code, name='scan_qr_code'),
     path('profile/', views.profile, name='profile'),
