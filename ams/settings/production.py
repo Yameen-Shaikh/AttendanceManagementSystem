b02b033d88a08df93e13e23e66350bd92269ebed
Tech-Wolf07 <yammu2005@gmail.com>
2025-11-23 19:28:46 +0530

    Secret key v1

--- ams/settings/production.py
+++ ams/settings/production.py
@@ -1,8 +1,5 @@
 from .base import *
 
-# TEMPORARY - Hardcoded secret key for debugging
-SECRET_KEY = 'temporary-secret-key-for-debugging-purposes-please-change'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
